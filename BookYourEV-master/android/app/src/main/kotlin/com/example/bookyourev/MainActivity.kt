ad435db58bd82899bbe39fb7ae3e156ac12878ae
ayush-4044 <ayushpatel1908@gmail.com>
2025-06-23 10:42:50 +0530

    Delete BookYourEV-master directory

--- BookYourEV-master/android/app/src/main/kotlin/com/example/bookyourev/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bookyourev
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- BookYourEV-master/lib/CommonWidget/ColorConst.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorConst{
-  static const Color black = Colors.black;
-
-}
